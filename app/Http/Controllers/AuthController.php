684839f577414020f256b219cd12dd4cb6aca6e1
Vinicius Koncicoski <viniciuskoncicoski@gmail.com>
2024-09-22 16:22:26 -0300

    feat: create logout route to forget user session

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -58,6 +58,7 @@ class AuthController extends Controller
 
     public function logout()
     {
-        return "logout";
+        session()->forget('user');
+        return redirect()->to('/login');
     }
 }
